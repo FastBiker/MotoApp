d55d40a2a956329f8fd0e183404ec1ec39301e89
Piotr Dyduch <padyduch@o2.pl>
2024-05-29 15:34:06 +0200

    Batch

--- MotoApp/MotoApp/Program.cs
+++ MotoApp/MotoApp/Program.cs
@@ -4,30 +4,52 @@ using MotoApp.Repositories;
 
 var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext());
 AddEmployees(employeeRepository);
-AddManagers(employeeRepository);
 WriteAllToConsole(employeeRepository);
 
 
-static void AddEmployees(IRepository<Employee> employeeRepository) 
+static void AddEmployees(IRepository<Employee> employeeRepository)
 {
-    employeeRepository.Add(new Employee { FirstName = "Adam" });
-    employeeRepository.Add(new Employee { FirstName = "Piotr" });
-    employeeRepository.Add(new Employee { FirstName = "Zuzia" });
-    employeeRepository.Save();
+    var employees = new[]
+    {
+        new Employee { FirstName = "Adam" },
+        new Employee { FirstName = "Piotr" },
+        new Employee { FirstName = "Zuzia" }
+    };
+
+    AddBatch(employeeRepository, employees);
+    //employeeRepository.Add(new Employee { FirstName = "Adam" });
+    //employeeRepository.Add(new Employee { FirstName = "Piotr" });
+    //employeeRepository.Add(new Employee { FirstName = "Zuzia" });
+    //employeeRepository.Save();
 }
 
-static void AddManagers(IWriteRepository<Manager> managerRepository)
+static void AddBusinessPartner(IRepository<BusinessPartner> businessPartnerRepository)
 {
-    managerRepository.Add(new Manager { FirstName = "Tomek" });
-    managerRepository.Add(new Manager { FirstName = "Przemek" });
-    managerRepository.Save();
+    var businessPartners = new[]
+    {
+        new BusinessPartner { FirstName = "Kleofas" },
+        new BusinessPartner { FirstName = "Hieronim" },
+        new BusinessPartner { FirstName = "Teofil" }
+    };
+
+    AddBatch(businessPartnerRepository, businessPartners);
+}
+static void AddBatch<T>(IRepository<T> repository, T[] items) 
+    where T : class, IEntity
+{
+    foreach (var item in items)
+    {
+        repository.Add(item);
+    }
+
+    repository.Save();
 }
 
-static void WriteAllToConsole(IReadRepository<IEntity> repository) 
+static void WriteAllToConsole(IReadRepository<IEntity> repository)
 {
-        var items = repository.GetAll();
-        foreach ( var item in items) 
-        {
-            Console.WriteLine(item);
-        }
+    var items = repository.GetAll();
+    foreach (var item in items)
+    {
+        Console.WriteLine(item);
+    }
 }
