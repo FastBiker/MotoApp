f043c5c360528477d8afa9bdbf4c1b2ac68758c3
Piotr Dyduch <padyduch@o2.pl>
2024-07-30 15:23:28 +0200

    Metoda GroupBy

--- MotoApp/MotoApp/App.cs
+++ MotoApp/MotoApp/App.cs
@@ -14,5 +14,22 @@ public class App : IApp
     {
         var cars = _csvReader.ProcessCars("Resourses\\Files\\fuel.csv");
         var manufacturers = _csvReader.ProcessManufacturers("Resourses\\Files\\manufacturers.csv");
+
+        var groups = cars
+            .GroupBy(x => x.Manufacture)
+            .Select(g => new
+            {
+                Name = g.Key,
+                Max = g.Max(c => c.Combined),
+                Average = g.Average(c => c.Combined)
+            })
+            .OrderBy(x => x.Name);
+
+        foreach ( var group in groups) 
+        {
+            Console.WriteLine($"{group.Name}");
+            Console.WriteLine($"\t Max: {group.Max}");
+            Console.WriteLine($"\t Average: {group.Average}");
+        }
     }
 }
