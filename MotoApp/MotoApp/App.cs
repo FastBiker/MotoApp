71ce8b054709f1c7780f56a5a6bf4543dd0c5a3b
Piotr Dyduch <padyduch@o2.pl>
2024-08-03 12:07:32 +0200

    Zapisywanie i odczytywanie plików xml

--- MotoApp/MotoApp/App.cs
+++ MotoApp/MotoApp/App.cs
@@ -1,9 +1,10 @@
-﻿using MotoApp.Components.CsvReader;
-using MotoApp.Components.CsvReader.Models;
-using MotoApp.Data.Entities;
-using System.Text.RegularExpressions;
+﻿namespace MotoApp;
 
-namespace MotoApp;
+using MotoApp.Components.CsvReader;
+using System;
+using System.Linq;
+using System.Xml.Linq;
+
 
 public class App : IApp
 {
@@ -15,71 +16,43 @@ public class App : IApp
 
     public void Run()
     {
-        var cars = _csvReader.ProcessCars("Resourses\\Files\\fuel.csv");
-        var manufacturers = _csvReader.ProcessManufacturers("Resourses\\Files\\manufacturers.csv");
+        CreateXml();
+        QueryXml();
+    }
 
-        var groups = manufacturers.GroupJoin(
-            cars,
-            manufacturer => manufacturer.Name,
-            car => car.Manufacturer,
-            (m, g) =>
-            new
-            {
-                Manufacturer = m,
-                Cars = g
-            })
-            .OrderBy(x => x.Manufacturer.Name); 
-        
-        foreach (var group in groups) 
+    private void QueryXml()
+    {
+        var document = XDocument.Load("fuel.xml");
+        var names = document
+            .Element("Cars")?
+            .Elements("Car")
+            .Where(x => x.Attribute("Displacement")?.Value == 6.0.ToString())
+            .Select(x => x.Attribute("Manufacturer")?.Value)
+            .Distinct()
+            .OrderDescending();
+
+        foreach (var name in names) 
         {
-            Console.WriteLine($"Manufacturer: {group.Manufacturer.Name}");
-            Console.WriteLine($"\t Count: {group.Cars.Count()}");
-            Console.WriteLine($"\t Max: {group.Cars.Max(x => x.Displacement)}");
-            Console.WriteLine($"\t Min: {group.Cars.Min(x => x.Displacement)}");
-            Console.WriteLine($"\t Average: {group.Cars.Average(x => x.Displacement)}");
-            Console.WriteLine();
-
+            Console.WriteLine(name);
         }
+        
+    }
 
+    private void CreateXml()
+    {
+        var records = _csvReader.ProcessCars("Resourses\\Files\\fuel.csv");
 
-        //var groups = cars
-        //    .GroupBy(x => x.Manufacturer)
-        //    .Select(g => new
-        //    {
-        //        Name = g.Key,
-        //        Max = g.Max(c => c.Combined),
-        //        Average = g.Average(c => c.Combined)
-        //    })
-        //    .OrderBy(x => x.Name);
+        var document = new XDocument();
 
-        //foreach ( var group in groups) 
-        //{
-        //    Console.WriteLine($"{group.Name}");
-        //    Console.WriteLine($"\t Max: {group.Max}");
-        //    Console.WriteLine($"\t Average: {group.Average}");
-        //}
+        var cars = new XElement("Cars", records
+            .Select(x =>
+                new XElement("Car",
+                    new XAttribute("Manufacturer", x.Manufacturer),
+                    new XAttribute("Name", x.Name),
+                    new XAttribute("Displacement", x.Displacement))));
 
-        //var carsInCountry = cars.Join(
-        //    manufacturers,
-        //    c => new { c.Manufacturer, c.Year },
-        //    m => new { Manufacturer = m.Name, m.Year },
-        //    (car, manufacturer) =>
-        //         new
-        //         {
-        //             manufacturer.Country,
-        //             manufacturer.Year,
-        //             car.Manufacturer,
-        //             car.Displacement
-        //         })
-        //    .OrderByDescending(x => x.Displacement)
-        //    .ThenBy(x => x.Manufacturer);
+        document.Add(cars);
+        document.Save("fuel.xml");
 
-        //foreach (var car in carsInCountry)
-        //{
-        //    Console.WriteLine($"Country: {car.Country}");
-        //    Console.WriteLine($"\t Name: {car.Manufacturer}");
-        //    Console.WriteLine($"\t Displacement: {car.Displacement}");
-        //    Console.WriteLine($"\t Year: {car.Year}");
-        //}
     }
 }
